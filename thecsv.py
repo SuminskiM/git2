b22af7d96a15f1d5f354c526b0c3c663d485f308
To ja <student-wroclaw@EDU-WSB.local>
2023-01-21 13:20:04 +0100

    Nowa wesja4

--- thecsv.py
+++ thecsv.py
@@ -2,5 +2,18 @@ path = 'C://Users//vdi-student//Desktop//git2/rozliczenie.csv'
 mode = 'r'
 with open (path, mode) as plik_csv:
     content = plik_csv.readlines()
-
+print(type(content))
+print(len(content))
 print(content)
+print(content[4])
+
+for i in range(len(content)):
+    content[i] = content[i].split(';')
+print(content)
+print(content[5])
+print(content[5][3])
+
+dane = 'mama.tata.pies'
+print(dane)
+dane = dane.split('.')
+print(dane)
